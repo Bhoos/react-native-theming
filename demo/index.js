38b50a7625c0a54094b5f0c2dd66c22852a80193
Ranjan Shrestha <ranjan@sharingapples.com>
2018-07-11 14:33:13 +0545

    Include ImageBackground component with Theme

--- demo/index.js
+++ demo/index.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
   View,
@@ -51,6 +51,10 @@ const styles = createStyle({
     color: '#888',
     marginBottom: 5,
   },
+  icon: {
+    width: 60,
+    height: 60,
+  },
   // The generic button and button can be one, only separated
   // here for testing purpose
   genericButton: {
@@ -71,35 +75,32 @@ const styles = createStyle({
 const Button = createThemedComponent(TouchableOpacity);
 const Bar = createThemedComponent(StatusBar, ['barStyle', 'backgroundColor']);
 
-export default class ThemeDemo extends Component {
-  render() {
-    return (
-      <Theme.View style={styles.container}>
-        <Bar barStyle="@statusBar" backgroundColor="@backgroundColor" />
-        <Theme.Image source="@icon" />
-        <Theme.Text style={styles.welcome}>
-          React Native Theming Demo!
-        </Theme.Text>
-        <Theme.Text style={styles.instructions}>
-          To experiment check app.js file
-        </Theme.Text>
-        <Text style={styles.instructions}>
-          You can now create your themes using JSON. The styles declaration
-          is directly compatible with StyleSheet.create. You just need to
-          replace `StyleSheet.create` with `createStyle` and add your theme
-          variables in the styles.
-        </Text>
-        <View style={{ flexDirection: 'row' }}>
-          { themes.map(theme => (
-            <Button key={theme.name} style={[styles.button, styles.genericButton]} onPress={() => theme.apply()}>
-              <Theme.Text style={[styles.buttonText, { color: '@buttonText' }]}>{theme.name}</Theme.Text>
-            </Button>
-            ))
-          }
-        </View>
-      </Theme.View>
-    );
-  }
-}
+
+const ThemeDemo = () => (
+  <Theme.View style={styles.container}>
+    <Bar barStyle="@statusBar" backgroundColor="@backgroundColor" />
+    <Theme.ImageBackground source="@icon" style={styles.icon} />
+    <Theme.Text style={styles.welcome}>
+      React Native Theming Demo!
+    </Theme.Text>
+    <Theme.Text style={styles.instructions}>
+      To experiment check app.js file
+    </Theme.Text>
+    <Text style={styles.instructions}>
+      You can now create your themes using JSON. The styles declaration
+      is directly compatible with StyleSheet.create. You just need to
+      replace `StyleSheet.create` with `createStyle` and add your theme
+      variables in the styles.
+    </Text>
+    <View style={{ flexDirection: 'row' }}>
+      { themes.map(theme => (
+        <Button key={theme.name} style={[styles.button, styles.genericButton]} onPress={() => theme.apply()}>
+          <Theme.Text style={[styles.buttonText, { color: '@buttonText' }]}>{theme.name}</Theme.Text>
+        </Button>
+        ))
+      }
+    </View>
+  </Theme.View>
+);
 
 AppRegistry.registerComponent('Foundation', () => ThemeDemo);
